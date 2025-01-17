21b355ca29422d2f9ce5779ce83ddae752068002
Hans-J. Schmid <knock@myopendoor.de>
2025-01-17 13:35:44 +0100

    01 - data model

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,17 +1,50 @@
 import collections
+from random import choice
 
-Card = collections.namedtuple('Card', ['rank', 'suit'])
+Card = collections.namedtuple("Card", ["rank", "suit"])
+
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2, 11)] + list('JQKA')
-    suits = 'spades diamonds clubs hearts'.split()
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
+    suits = "spades diamonds clubs hearts".split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits
-                                        for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
 
     def __getitem__(self, position):
         return self._cards[position]
+
+    # basedpyright complains without it (__getitem__ is not sufficient):
+    def __iter__(self):
+        return self._cards.__iter__()
+
+
+def spades_high(card):
+    suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+
+if __name__ == "__main__":
+    beer_card = Card("7", "diamonds")
+    print(f"Just an ordinary card: {beer_card}")
+    deck = FrenchDeck()
+    print(f"Number of cards in deck: {len(deck)}")
+    print(f"Lowest card: {deck[0]}")
+    print(f"Highest card: {deck[-1]}")
+    print(f"Just a random card: {choice(deck)}")
+    print(f"Three lowest cards: {deck[:3]}")
+    print(f"All aces: {deck[12::13]}")
+    for card in deck:
+        print(card)
+    for card in reversed(deck):
+        print(card)
+    print(Card("Q", "hearts") in deck)
+    print(Card("7", "beasts") in deck)
+    print(spades_high(beer_card))
+    print("Sorted card deck:")
+    for card in sorted(deck, key=spades_high):
+        print(card)
--- 01-data-model/vector2d.py
+++ 01-data-model/vector2d.py
@@ -28,17 +28,16 @@ Scalar multiplication::
 
 """
 
-
 import math
 
-class Vector:
 
-    def __init__(self, x=0, y=0):
+class Vector:
+    def __init__(self, x=0.0, y=0.0):
         self.x = x
         self.y = y
 
     def __repr__(self):
-        return f'Vector({self.x!r}, {self.y!r})'
+        return f"Vector({self.x!r}, {self.y!r})"
 
     def __abs__(self):
         return math.hypot(self.x, self.y)
@@ -51,5 +50,15 @@ class Vector:
         y = self.y + other.y
         return Vector(x, y)
 
-    def __mul__(self, scalar):
+    def __mul__(self, scalar: float):
         return Vector(self.x * scalar, self.y * scalar)
+
+
+if __name__ == "__main__":
+    v1 = Vector(3, 4)
+    v2 = Vector(2, 1)
+    v_res = v1 + v2
+    print(v_res)
+    print(v1 * 3)
+    print(abs(v1))
+    print(abs(v1 * 3))
